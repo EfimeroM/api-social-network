00706c6e5c2a6810a1086b21f98cda46cd8fc41c
EfimeroM <martinperez9713@gmail.com>
2024-02-02 16:21:37 -0300

    feat: add routes & controllers: user , publication, follow

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const connectionDb = require("./database/connection")
 const express = require("express")
 const cors = require("cors")
 const { PORT } = require("./config")
+const UserRoutes = require("./routes/user")
+const PublicationRoutes = require("./routes/publication")
+const FollowRoutes = require("./routes/follow")
 
 connectionDb()
 
@@ -10,5 +13,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use("/api", UserRoutes)
+app.use("/api", PublicationRoutes)
+app.use("/api", FollowRoutes)
 
 app.listen(PORT, ()=> console.log("Initialized server on port:", PORT))
