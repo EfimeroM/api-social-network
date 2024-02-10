4db1a61a6fbcfb400d8c5e0c2ed76e2dd38b2ef6
EfimeroM <martinperez9713@gmail.com>
2024-02-10 12:39:34 -0300

    feat: add unfollow in routes & controllers

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,3 +1,30 @@
 const Follow = require("../models/Follow")
 const User = require("../models/User")
 
+const save = async (req, res) => {
+  const params = req.body
+  const identity = req.user
+  const userToFollow = new Follow({
+    user: identity._id,
+    followed: params.followed
+  })
+
+  const followStored = await userToFollow.save()
+  if (!followStored) return res.status(400).send({ status: "error", message: "Error to follow user" })
+  return res.status(200).send({ status: "success", message: "Success followed user", identity: req.user, follow: followStored })
+}
+
+const unfollow = async (req, res) => {
+  const userId = req.user._id
+  const followedId = req.params.id
+
+  const followDeleted = await Follow.findOneAndDelete({ user: userId, followed: followedId })
+  if (!followDeleted) return res.status(404).send({ status: "error", message: "User followed not found" })
+
+  return res.status(200).send({ status: "success", message: "Success deleted follow" })
+}
+
+module.exports = {
+  save,
+  unfollow
+}
--- routes/follow.js
+++ routes/follow.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const FollowController = require("../controllers/follow")
+const check = require("../middlewares/auth")
+
+router.post("/save", check.auth, FollowController.save)
+router.delete("/unfollow/:id", check.auth, FollowController.unfollow)
 
 module.exports = router
